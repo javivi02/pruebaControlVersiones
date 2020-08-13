617dfd8b41f0b2d6be528cb5af6a3be1d72fdad5
javivi02 <65453001+javivi02@users.noreply.github.com>
2020-08-13 13:14:32 +0200

    todos los movimientos de fichas implementados, tablero de 8*8 creado. hay que ajustar movimientos sonbre las fichas

--- src/ajedrez/movimientos.java
+++ src/ajedrez/movimientos.java
@@ -1,6 +1,5 @@
 package ajedrez;
 
-import com.sun.jndi.toolkit.ctx.Continuation;
 import java.util.ArrayList;
 
 /**
@@ -8,7 +7,12 @@ import java.util.ArrayList;
  */
 public class movimientos {
     
-//    private tablero tablero;
+    private tablero tablero;
+    
+    public movimientos(){
+        
+        tablero = new tablero();
+    }
     
     public ArrayList <String> moverCaballo(String coordenada){
         
@@ -18,44 +22,36 @@ public class movimientos {
         
         int fila = Integer.parseInt(temporal[1]);
         int columna = Integer.parseInt(temporal[2]);
-        
-        if(fila == 0 && columna == 0){
-            resultado.add("p" + (fila + 2) + (columna + 1));
-            resultado.add("p" + (fila + 1) + (columna + 2));
-        }
-        else if(fila == 0 && columna == 1){
-            resultado.add("p" + (fila + 2) + (columna + 1));
-            resultado.add("p" + (fila + 2) + (columna - 1));
-        }
-        else if(fila == 0 && columna == 2){
-            resultado.add("p" + (fila + 2) + (columna - 1));
-            resultado.add("p" + (fila + 1) + (columna - 2));
-        }
-        else if(fila == 1 && columna == 0){
-            resultado.add("p" + (fila - 1) + (columna + 2));
-            resultado.add("p" + (fila + 1) + (columna + 2));
-        }
-        else if(fila == 1 && columna == 2){
-            resultado.add("p" + (fila - 1) + (columna - 2));
-            resultado.add("p" + (fila + 1) + (columna - 2));
-        }
-        else if(fila == 2 && columna == 0){
-            resultado.add("p" + (fila - 2) + (columna + 1));
-            resultado.add("p" + (fila - 1) + (columna + 2));
-        }
-        else if(fila == 2 && columna == 1){
-            resultado.add("p" + (fila - 2) + (columna + 1));
+
+        if ((fila - 2 >= 0 && fila - 2 < tablero.FILAS) && (columna - 1 >= 0 && columna - 1 < tablero.COLUMNAS)) {
             resultado.add("p" + (fila - 2) + (columna - 1));
         }
-        else if(fila == 2 && columna == 2){
-            resultado.add("p" + (fila - 2) + (columna - 1));
+        if ((fila - 2 >= 0 && fila - 2 < tablero.FILAS) && (columna + 1 >= 0 && columna + 1 < tablero.COLUMNAS)) {
+            resultado.add("p" + (fila - 2) + (columna + 1));
+        }
+        if ((fila - 1 >= 0 && fila - 1 < tablero.FILAS) && (columna + 2 >= 0 && columna + 2 < tablero.COLUMNAS)) {
+            resultado.add("p" + (fila - 1) + (columna + 2));
+        }
+        if ((fila + 1 >= 0 && fila + 1 < tablero.FILAS) && (columna + 1 >= 0 && columna + 2 < tablero.COLUMNAS)) {
+            resultado.add("p" + (fila + 1) + (columna + 2));
+        }
+        if ((fila + 2 >= 0 && fila + 2 < tablero.FILAS) && (columna + 1 >= 0 && columna + 1 < tablero.COLUMNAS)) {
+            resultado.add("p" + (fila + 2) + (columna + 1));
+        }
+        if ((fila + 2 >= 0 && fila + 2 < tablero.FILAS) && (columna - 1 >= 0 && columna - 1 < tablero.COLUMNAS)) {
+            resultado.add("p" + (fila + 2) + (columna - 1));
+        }
+        if ((fila + 1 >= 0 && fila + 1 < tablero.FILAS) && (columna - 2 >= 0 && columna - 2 < tablero.COLUMNAS)) {
+            resultado.add("p" + (fila + 1) + (columna - 2));
+        }
+        if ((fila - 1 >= 0 && fila - 1 < tablero.FILAS) && (columna - 2 >= 0 && columna - 2 < tablero.COLUMNAS)) {
             resultado.add("p" + (fila - 1) + (columna - 2));
         }
-        
+
         return resultado;
     }
     
-    public ArrayList <String> moverPeon (String coordenada){
+    public ArrayList <String> moverPeonBlanco (String coordenada){
         
         ArrayList <String> resultado = new ArrayList<>();
         
@@ -64,7 +60,30 @@ public class movimientos {
         int fila = Integer.parseInt(temporal[1]);
         int columna = Integer.parseInt(temporal[2]);
         
-        if(fila > 0) resultado.add("p" + (fila - 1) + columna);
+//        if(fila > 0) resultado.add("p" + (fila - 1) + columna);
+
+        //posible movimiento arriba
+        if (fila - 1 >= 0) {
+            resultado.add("p" + (fila - 1) + columna);
+        }
+
+        return resultado;
+        
+    }
+    
+    public ArrayList <String> moverPeonNegro (String coordenada){
+        
+        ArrayList <String> resultado = new ArrayList<>();
+        
+        String temporal [] = coordenada.split("");
+        
+        int fila = Integer.parseInt(temporal[1]);
+        int columna = Integer.parseInt(temporal[2]);
+
+        //posible movimientos abajo
+        if (fila + 1 < tablero.COLUMNAS) {
+            resultado.add("p" + (fila + 1) + columna);
+        }
 
         return resultado;
         
@@ -79,6 +98,95 @@ public class movimientos {
         int fila = Integer.parseInt(temporal[1]);
         int columna = Integer.parseInt(temporal[2]);
         
+        
+        //posibles movimientos izquierda
+        for (int i = columna - 1; i >= 0; i--) {
+            if(filtroPiezasBlancas("p" + fila + i)) break;
+            resultado.add("p" + fila + i);
+        }
+        
+        //posibles movimientos derecha
+        for (int i = columna + 1 ; i < tablero.FILAS; i++) {
+            if(filtroPiezasBlancas("p" + fila + i)) break;
+            resultado.add("p" + fila + i);
+        }
+        
+        //posibles movimientos arriba
+        for (int i = fila - 1; i >= 0; i--) {
+            if(filtroPiezasBlancas("p" + fila + i)) break;
+            resultado.add("p" + i + columna);
+        }
+        
+        //posibles movimientos abajo
+        for (int i = fila + 1; i < tablero.COLUMNAS; i++) {
+            if(filtroPiezasBlancas("p" + fila + i)) break;
+            resultado.add("p" + i + columna);
+        }
+
+        return resultado;
+    }
+    
+    public ArrayList <String> moverAlfil (String coordenada){
+
+        ArrayList <String> resultado = new ArrayList<>();
+        
+        String temporal [] = coordenada.split("");
+        
+        int fila = Integer.parseInt(temporal[1]);
+        int columna = Integer.parseInt(temporal[2]);
+
+        //posibles movimientos derecha abajo
+        for (int i = 1; fila + i < tablero.FILAS && columna - i >= 0; i++) {
+            resultado.add("p" + (fila + i) + (columna - i)); 
+        }
+        
+        //posibles movimientos izquierda abajo
+        for (int i = 1; fila + i < tablero.FILAS && columna + i < tablero.COLUMNAS; i++) {
+            resultado.add("p" + (fila + i) + (columna + i)); 
+        }
+        
+        //posibles movimientos derecha arriba
+        for (int i = 1; fila - i >= 0 && columna + i < tablero.COLUMNAS; i++) {
+            resultado.add("p" + (fila - i) + (columna + i)); 
+        }
+        
+        //posibles movimientos izquierda arriba
+        for (int i = 1; fila - i >= 0 && columna - i >= 0; i++) {
+            resultado.add("p" + (fila - i) + (columna - i)); 
+        }
+        
+        return resultado;
+        
+    }
+    
+    public ArrayList <String> moverDama (String coordenada){
+
+        ArrayList <String> resultado = new ArrayList<>();
+        
+        String temporal [] = coordenada.split("");
+        
+        int fila = Integer.parseInt(temporal[1]);
+        int columna = Integer.parseInt(temporal[2]);
+
+        //posibles movimientos derecha abajo
+        for (int i = 1; fila + i < tablero.FILAS && columna - i >= 0; i++) {
+            resultado.add("p" + (fila + i) + (columna - i)); 
+        }
+        
+        //posibles movimientos izquierda abajo
+        for (int i = 1; fila + i < tablero.FILAS && columna + i < tablero.COLUMNAS; i++) {
+            resultado.add("p" + (fila + i) + (columna + i)); 
+        }
+        
+        //posibles movimientos derecha arriba
+        for (int i = 1; fila - i >= 0 && columna + i < tablero.COLUMNAS; i++) {
+            resultado.add("p" + (fila - i) + (columna + i)); 
+        }
+        
+        //posibles movimientos izquierda arriba
+        for (int i = 1; fila - i >= 0 && columna - i >= 0; i++) {
+            resultado.add("p" + (fila - i) + (columna - i)); 
+        }
         
         //posibles movimientos izquierda
         for (int i = columna - 1; i >= 0; i--) {
@@ -99,22 +207,98 @@ public class movimientos {
         for (int i = fila + 1; i < tablero.COLUMNAS; i++) {
             resultado.add("p" + i + columna);
         }
-
+        
         return resultado;
+        
+    }
+    
+    public ArrayList <String> moverRey (String coordenada){
+
+        ArrayList <String> resultado = new ArrayList<>();
+        
+        String temporal [] = coordenada.split("");
+        
+        int fila = Integer.parseInt(temporal[1]);
+        int columna = Integer.parseInt(temporal[2]);
+
+        //posible movimientos derecha abajo
+        if (fila + 1 < tablero.FILAS && columna - 1 >= 0) {
+            resultado.add("p" + (fila + 1) + (columna - 1)); 
+        }
+        
+        //posible movimientos izquierda abajo
+        if(fila + 1 < tablero.FILAS && columna + 1 < tablero.COLUMNAS) {
+            resultado.add("p" + (fila + 1) + (columna + 1)); 
+        }
+        
+        //posible movimientos derecha arriba
+        if(fila - 1 >= 0 && columna + 1 < tablero.COLUMNAS) {
+            resultado.add("p" + (fila - 1) + (columna + 1)); 
+        }
+        
+        //posible movimientos izquierda arriba
+        if(fila - 1 >= 0 && columna - 1 >= 0) {
+            resultado.add("p" + (fila - 1) + (columna - 1)); 
+        }
+        
+        //posible movimientos izquierda
+        if (columna - 1 >= 0) {
+            resultado.add("p" + fila + (columna - 1));
+        }
+        
+        //posible movimientos derecha
+        if (columna + 1 < tablero.FILAS) {
+            resultado.add("p" + fila + (columna + 1));
+        }
+        
+        //posible movimientos arriba
+        if (fila - 1 >= 0) {
+            resultado.add("p" + (fila - 1) + columna);
+        }
+        
+        //posible movimientos abajo
+        if (fila + 1 < tablero.COLUMNAS) {
+            resultado.add("p" + (fila + 1) + columna);
+        }
+        
+        return resultado;
+        
+    }
+    
+    private boolean filtroPiezasBlancas(String valorCoordenada){
+        
+        ArrayList <String> temporal = tablero.getCoordenadasPiezasBlancas();
+        
+        for (int i = 0; i < temporal.size(); i++) {
+            if(temporal.get(i).equalsIgnoreCase(valorCoordenada)) return true;
+        }
+        
+        return false;
+        
     }
     
     
     public static void main(String[] args) {
-        
+         
         movimientos movimientos = new movimientos();
         
-        ArrayList <String> listar = movimientos.moverTorre("p21");
+        ArrayList <String> prueba = movimientos.moverTorre("p70");
         
-        for (int i = 0; i < listar.size(); i++) {
-            System.out.print(listar.get(i) + " ");
+        for (int i = 0; i < prueba.size(); i++) {
+            System.out.print(prueba.get(i) + " ");
         }
         
-//        System.out.println(movimientos.moverPeon("p01"));
+//        movimientos.filtroPiezasBlancas("");
+//
+//        System.out.println(movimientos.moverPeonBlanco("p00"));
+
+        System.out.println("------------------------------");
         
+        tablero tablero = new tablero();
+        ArrayList <String> listar = tablero.getCoordenadasPiezasBlancas();
+
+        for (int i = 0; i < listar.size(); i++) {
+            System.out.println(listar.get(i));
+        } 
     }
 }
